2693a37d201c190eea92ec19fceac37764a533cf
Nishit278 <nishitbnsl@gmail.com>
2021-10-20 09:50:19 +0530

    made some changes

--- bot.js
+++ bot.js
@@ -1,7 +1,8 @@
 require("dotenv").config();
 const Discord = require("discord.js");
-const { getMemes, getNews, getCats } = require("./posts.js");
+const { getMemes, getNews, getPets } = require("./posts.js");
 const { authSpotify, searchSpotify } = require("./spotify.js");
+const { play } = require("./play.js");
 const client = new Discord.Client({
   partials: ["CHANNEL", "MESSAGE"],
   restTimeOffset: 0,
@@ -10,25 +11,29 @@ const client = new Discord.Client({
 const token = process.env.BOT_TOKEN;
 // const fetch = require("node-fetch");
 // import fetch from "node-fetch";
-const PREFIX = "!r";
+const PREFIX = ".r";
 
 client.on("messageCreate", async (message) => {
   if (message.author.bot) return;
+  // const currMember = Guild.members.cache.get("UserID");
+
   if (message.content.startsWith(PREFIX)) {
     const [cmdName, ...args] = message.content
       .substring(PREFIX.length)
       .split(/\s+/);
+    const cmd = args[0];
+    const secondArg = args.slice(1).join(" ");
     // console.log(cmdName, args);
-    if (args[0] === "help") {
+    if (cmd === "help") {
       let embed = new Discord.MessageEmbed()
         .setColor("#6D1FFF")
-        .setTitle("Redify bot's commands")
+        .setTitle("Reddify bot's commands")
         .setThumbnail(
           "https://icons-for-free.com/iconfiles/png/512/reddit+website+icon-1320168605279647340.png"
-      )
+        )
         .setTimestamp()
         .addFields(
-          { name: "Prefix", value: "!r", inline: false },
+          { name: "Prefix", value: ".r", inline: false },
           { name: "Reddit", value: "<Prefix> <Value>", inline: false },
           { name: "Values", value: "meme\nnews\ncats", inline: false },
           { name: "Music", value: "<Prefix> <Flag> <Value>", inline: false },
@@ -38,7 +43,7 @@ client.on("messageCreate", async (message) => {
             value: "Track\nAlbum\nPlaylist\nArtist",
             inline: true,
           },
-          {name: "\u200B", value: "\u200B"}
+          { name: "\u200B", value: "\u200B" }
         );
 
       // .setURL(song.external_urls.spotify)
@@ -47,7 +52,7 @@ client.on("messageCreate", async (message) => {
       // .addField("Followers: ", song.followers.total.toString());
       message.channel.send({ embeds: [embed] });
     }
-    if (args[0] === "meme") {
+    if (cmd === "meme") {
       const random = Math.floor(Math.random() * 20);
       const post = await getMemes(random);
       // console.log(post.title)
@@ -61,99 +66,96 @@ client.on("messageCreate", async (message) => {
       // console.log(post);
       message.channel.send({ embeds: [embed] });
     }
-    if (args[0] === "news") {
-      const random = Math.floor(Math.random() * 20);
+    if (cmd === "news") {
+      const random = Math.floor(Math.random() * 10);
       const post = await getNews(random);
-      // console.log(post.title)
-      const embed = new Discord.MessageEmbed()
-        .setColor("#ff4500")
-        .setTitle(post.title)
-        .setURL("https://www.reddit.com" + post.permalink)
-        .setThumbnail(post.thumbnail)
-        .setTimestamp()
-        .setFooter(`👍 ${post.ups} | 💬 ${post.num_comments}`);
-      // console.log(post);
-      message.channel.send({ embeds: [embed] });
+      console.log(post);
+      message.channel.send(post.url);
+      // const embed = new Discord.MessageEmbed()
+      //   .setColor("#ff4500")
+      //   .setTitle(post.title)
+      //   .setURL("https://www.reddit.com" + post.permalink)
+      //   .setThumbnail(post.thumbnail)
+      //   .setTimestamp()
+      //   .setFooter(`👍 ${post.ups} | 💬 ${post.num_comments}`);
+      // // console.log(post);
+      // message.channel.send({ embeds: [embed] });
       // message.channel.send(post.title, { files: [post.url] });
     }
-    if (args[0] === "cats") {
-      const random = Math.floor(Math.random() * 20);
-      const post = await getCats(random);
-      // console.log(post);
-      if (post.is_video || post.post_hint) {
-        const embed = new Discord.MessageEmbed()
-          .setColor("#ff4500")
-          .setTitle(post.title)
-          .setURL("https://www.reddit.com" + post.permalink)
-          .setThumbnail(post.thumbnail)
-          .setTimestamp()
-          .setFooter(`👍 ${post.ups} | 💬 ${post.num_comments}`);
-        message.channel.send({ embeds: [embed] });
-      } else {
-        const embed = new Discord.MessageEmbed()
-          .setTitle(post.title)
-          .setURL("https://www.reddit.com" + post.permalink)
-          .setImage(post.url)
-          .setFooter(`👍 ${post.ups} | 💬 ${post.num_comments}`);
-        message.channel.send({ embeds: [embed] });
-      }
-    }
-    if (
-      args[0] === "tr" ||
-      args[0] === "ar" ||
-      args[0] === "al" ||
-      args[0] == "pl"
-    ) {
-      const auth = await authSpotify();
-      const query = args.slice(1).join(" ");
-      // console.log(query);
-      const song = await searchSpotify(auth, args[0], query);
+    if (cmd === "img") {
+      // console.log(secondArg);
+      try {
+        const random = Math.floor(Math.random() * 20);
+      const post = await getPets(random, secondArg);
 
-      if (args[0] !== "ar" && args[0] !== "pl") {
-        const artist = [];
-        song.artists.forEach((a) => artist.push(a.name));
-        const artists = artist.slice(0).join(", ");
-        // console.log(song);
-        if (args[0] === "al") {
+      const embed = new Discord.MessageEmbed()
+        .setColor("#977fd7")
+        .setTitle(`Uploaded by ${post.user}`)
+        .setURL(post.pageURL)
+        .setImage(post.webformatURL)
+        .setTimestamp()
+        .setFooter(`👍 ${post.likes} | 💬 ${post.comments}`);
+      message.channel.send({ embeds: [embed] });
+      } catch (err) {
+        message.channel.send("Couldn't find any related images")
+      }
+      
+    }
+    if (cmd === "tr" || cmd === "ar" || cmd === "al" || cmd == "pl") {
+      try {
+        const auth = await authSpotify();
+        const query = args.slice(1).join(" ");
+        // console.log(query);
+        const song = await searchSpotify(auth, cmd, query);
+        if (cmd !== "ar" && cmd !== "pl") {
+          const artist = [];
+          song.artists.forEach((a) => artist.push(a.name));
+          const artists = artist.slice(0).join(", ");
+          // console.log(song);
+          if (cmd === "al") {
+            let embed = new Discord.MessageEmbed()
+              .setColor("#1DB954")
+              .setTitle(song.name)
+              .setThumbnail(song.images[0].url)
+              .setURL(song.external_urls.spotify)
+              .addField("Artist(s)", artists);
+            message.channel.send({ embeds: [embed] });
+          } else {
+            try {
+              await message.channel.send(song.external_urls.spotify);
+            } catch (err) {
+              message.channel.send(err);
+            }
+          }
+        } else if (cmd === "pl") {
+          // console.log(song);
+          let embed = new Discord.MessageEmbed()
+            .setColor("#1DB954")
+            .setTitle(song.name)
+            .setDescription(song.description)
+            .setThumbnail(song.images[0].url)
+            .setURL(song.external_urls.spotify)
+            .addField("songs", song.tracks.total.toString())
+            .addField("Owner", song.owner.display_name);
+          message.channel.send({ embeds: [embed] });
+        } else {
+          console.log(song);
           let embed = new Discord.MessageEmbed()
             .setColor("#1DB954")
             .setTitle(song.name)
             .setThumbnail(song.images[0].url)
             .setURL(song.external_urls.spotify)
-            .addField("Artist(s)", artists);
-          message.channel.send({ embeds: [embed] });
-        } else {
-          let embed = new Discord.MessageEmbed()
-            .setColor("#1DB954")
-            .setTitle(song.name)
-            .setThumbnail(song.album.images[0].url)
-            .setURL(song.external_urls.spotify)
-            .addField("Artist(s)", artists);
+            .addField("Popularity: ", song.popularity.toString())
+            .addField("Genres: ", song.genres.join(", "))
+            .addField("Followers: ", song.followers.total.toString());
           message.channel.send({ embeds: [embed] });
         }
-      } else if (args[0] === "pl") {
-        // console.log(song);
-        let embed = new Discord.MessageEmbed()
-          .setColor("#1DB954")
-          .setTitle(song.name)
-          .setDescription(song.description)
-          .setThumbnail(song.images[0].url)
-          .setURL(song.external_urls.spotify)
-          .addField("songs", song.tracks.total.toString())
-          .addField("Owner", song.owner.display_name);
-        message.channel.send({ embeds: [embed] });
-      } else {
-        console.log(song);
-        let embed = new Discord.MessageEmbed()
-          .setColor("#1DB954")
-          .setTitle(song.name)
-          .setThumbnail(song.images[0].url)
-          .setURL(song.external_urls.spotify)
-          .addField("Popularity: ", song.popularity.toString())
-          .addField("Genres: ", song.genres.join(", "))
-          .addField("Followers: ", song.followers.total.toString());
-        message.channel.send({ embeds: [embed] });
+      } catch (error) {
+        message.channel.send("Please check for typos and try again :)");
       }
+    }
+    if (cmd === "play") {
+      play(message, cmd, args);
     }
   }
 });
--- posts.js
+++ posts.js
@@ -1,30 +1,32 @@
 const axios = require("axios");
+require("dotenv").config();
+const pixabayKey = process.env.PIXABAY_API;
 
-const getMemes = async (num)=>{
-    const subreddits = ["memes", "wholesomememes", "dankmemes"];
-    const x = Math.floor(Math.random()*subreddits.length);
-    const post = await axios.get(`https://www.reddit.com/r/${subreddits[x]}/hot.json`);
-    // console.log(post);
-    const postArray = post.data.data.children;
-    return postArray[num+1].data;
+const getMemes = async (num) => {
+  const subreddits = ["memes", "wholesomememes", "dankmemes"];
+  const x = Math.floor(Math.random() * subreddits.length);
+  const post = await axios.get(
+    `https://www.reddit.com/r/${subreddits[x]}/hot.json`
+  );
+  // console.log(post);
+  const postArray = post.data.data.children;
+  return postArray[num + 1].data;
+};
+const getNews = async (num) => {
+  const subreddits = ["technology", "tech", "worldnews", "engineering"];
+  const x = Math.floor(Math.random() * subreddits.length);
+  const post = await axios.get(
+    `https://www.reddit.com/r/${subreddits[x]}/hot.json`
+  );
+  const postArray = post.data.data.children;
+  // console.log(postArray[num+1]);
+  return postArray[num + 2].data;
+};
+const getPets = async (num, search) => {
+  const post = await axios.get(
+    `https://pixabay.com/api/?key=${pixabayKey}&q=${search}&image_type=photo&pretty=true`
+  );
+    return post.data.hits[num];
+};
 
-}
-const getNews = async (num)=>{
-    const subreddits = ["technology", "tech", "worldnews", "news"];
-    const x = Math.floor(Math.random()*subreddits.length);
-    const post = await axios.get(`https://www.reddit.com/r/${subreddits[x]}/hot.json`);
-    const postArray = post.data.data.children;
-    // console.log(postArray[num+1]);
-    return postArray[num+1].data;
-
-}
-const getCats = async (num)=>{
-    const subreddits = ["cat", "Kitten"];
-    const x = Math.floor(Math.random()*subreddits.length);
-    const post = await axios.get(`https://www.reddit.com/r/${subreddits[x]}/hot.json`);
-    const postArray = post.data.data.children;
-    return postArray[num+1].data;
-
-}
-
-module.exports = {getMemes, getNews, getCats};
+module.exports = { getMemes, getNews, getPets };
